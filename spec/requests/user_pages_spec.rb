d0a71f66878d44e577ddd1d427f51673183b5f71
ktrtk <kttktkkt@gmail.com>
2013-07-15 02:29:14 +0900

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -39,7 +39,7 @@ describe "UserPages" do
 			end
 
 			it "should create a user" do
-				epect { click_button submit }.to change(User, :count).by(1)
+				expect { click_button submit }.to change(User, :count).by(1)
 			end
 		end
 	end
